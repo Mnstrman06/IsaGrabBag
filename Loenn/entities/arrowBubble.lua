4f7eee1050f248fb479c5f03190e8011d394bf12
Mnstrman06 <52503167+Mnstrman06@users.noreply.github.com>
2022-10-17 17:01:18 -0400

    finally fix arrow bubble
    currently has a placeholder texture of the regular red bubble, i do not know enough about lua to make it change based on the direction of the bubble

--- Loenn/entities/arrowBubble.lua
+++ Loenn/entities/arrowBubble.lua
@@ -4,21 +4,25 @@ arrowBubble.name = "isaBag/arrowBubble"
 arrowBubble.depth = -100
 arrowBubble.placements = {
     {
+        {
         name = "left",
         data = {
             direction = "left"
         }
     },
+        {
         name = "right",
         data = {
             direction = "right"
         }
     },
+        {
         name = "up",
         data = {
             direction = "up"
         }
     },
+        {
         name = "down",
         data = {
             direction = "down"
